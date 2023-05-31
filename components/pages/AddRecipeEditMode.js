a6a62792ebb0cb6b92a403e9f154c3a28dc4c5b7
timmno12 <benjamintimm21@gmail.com>
2023-05-31 15:53:20 +0200

    added ingredientsadding function / update of view missing

--- components/pages/AddRecipeEditMode.js
+++ components/pages/AddRecipeEditMode.js
@@ -13,16 +13,51 @@ import {
 } from "react-native";
 
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { Picker } from "@react-native-picker/picker";
 
 export default function AddRecipeEditMode({
-  tempRecipe = {link: ""},
+  tempRecipe = { link: "" },
   setTempRecipe,
 }) {
   const [mode, setMode] = useState("ingredients");
-
+  const [selectedUnit, setSelectedUnit] = useState('g');
+  const [ingredientValue, setIngredientValue] = useState('');
+  const [amountValue, setAmountValue] = useState();
   const changeMode = (newMode) => {
     setMode(newMode);
   };
+
+  const ingredients = [];
+
+  const addIngredient = () => {
+    if (ingredientValue !== '' && amountValue !== '') {
+      ingredients.push({
+        ingredient: ingredientValue,
+        amount: amountValue,
+        unit: selectedUnit,
+      });
+      setIngredientValue('');
+      setAmountValue('');
+
+      console.log('ingredient added')
+    }	
+  };
+
+  const generateIngredientList = () => {
+    ingredients.map((ingredient, index) => {
+      return (
+        <View style={{ height: 100 }} key={index}>
+          <Text>
+            {ingredient.amount} {ingredient.unit}{" "}
+            {ingredient.ingredient}
+          </Text>
+          <Button title="Edit" />
+          <Button title="Delete" />
+        </View>
+      );
+    })
+  };
+
   if (tempRecipe.link !== "") {
     return (
       <View style={{ flex: 1 }}>
@@ -132,34 +167,101 @@ export default function AddRecipeEditMode({
               {/* Zutaten View */}
               <View style={styles.ingredientsContainer}>
                 <Text style={styles.textSize}>Zutaten</Text>
-                <SafeAreaView style={{ padding: 10 }}>
-                  <ScrollView>
-                    <View style={{ flexDirection: "row" }}>
-                      <TouchableOpacity>
-                        <Ionicons name="pencil" size={25} color="black" />
-                      </TouchableOpacity>
-                      <TextInput
-                        style={styles.textSize}
-                        placeholder="Zutat hinzufügen"
-                      />
-                    </View>
-                  </ScrollView>
+                <SafeAreaView>
+                  <View style={{ flexDirection: "row" }}>
+                    <TouchableOpacity onPress={addIngredient}
+                      style={{
+                        justifyContent: "center",
+                        alignContent: "center",
+                      }}
+                    >
+                      <Ionicons name="pencil" size={25} color="black" />
+                    </TouchableOpacity>
+                    <TextInput
+                      style={{ fontSize: 20, textAlign: "center" }}
+                      placeholder="Zutat hinzufügen"
+                      value={ingredientValue}
+                      onChangeText={(text) => setIngredientValue(text)}
+                    />
+                    <TextInput
+                      inputMode="numeric"
+                      style={{ fontSize: 20, textAlign: "center" }}
+                      value={amountValue}
+                      onChangeText={(text) => setAmountValue(text)}
+                      placeholder="Menge"
+                    />
+                    <Picker
+                      style={{ height: 50, width: 130 }}
+                      selectedValue={selectedUnit}
+                      onValueChange={(itemValue, itemIndex) =>
+                        setSelectedUnit(itemValue)
+                      }
+                    >
+                      <Picker.Item label="g" value="g" />
+                      <Picker.Item label="kg" value="kg" />
+                      <Picker.Item label="ml" value="ml" />
+                      <Picker.Item label="L" value="L" />
+                      <Picker.Item label="Stk" value="Stk" />
+                      <Picker.Item label="TL" value="TL" />
+                      <Picker.Item label="EL" value="EL" />
+                      <Picker.Item label="Prise" value="Prise" />
+                      <Picker.Item label="Msp" value="Msp" />
+                      <Picker.Item label="Bund" value="Bund" />
+                      <Picker.Item label="Packung" value="Packung" />
+                      <Picker.Item label="Dose" value="Dose" />
+                      <Picker.Item label="Becher" value="Becher" />
+                      <Picker.Item label="Tasse" value="Tasse" />
+                      <Picker.Item label="Glas" value="Glas" />
+                      <Picker.Item label="Flasche" value="Flasche" />
+                      <Picker.Item label="Scheibe" value="Scheibe" />
+                    </Picker>
+                  </View>
                 </SafeAreaView>
               </View>
             </View>
           ) : (
-            <View style={{paddingTop: 30, paddingLeft: 10}}>
+            <View style={{ paddingTop: 30, paddingLeft: 10 }}>
               <View style={{ flexDirection: "row" }}>
-                <TouchableOpacity>
+                <TouchableOpacity
+                >
                   <Ionicons name="pencil" size={25} color="black" />
                 </TouchableOpacity>
                 <TextInput
+                  value={ingredientValue}
+                  onChangeText={(text) => setIngredientValue(text)}
                   style={styles.textSize}
                   placeholder="Zutat hinzufügen"
                 />
               </View>
+              <View>
+                <ScrollView
+                  style={{}}
+                  contentContainerStyle={{ marginBottom: 5 }}
+                >
+                  {console.log(ingredients)}
+                </ScrollView>
+              </View>
             </View>
           )}
+        </View>
+        <View
+          style={{
+            flex: 0.2,
+            backgroundColor: "pink",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingLeft: 10,
+          }}
+        >
+          <TouchableOpacity>
+            <Ionicons name="trash-outline" size={30} color="black" />
+          </TouchableOpacity>
+
+          <Text style={styles.textSize}>Edit Mode</Text>
+          <TouchableOpacity>
+            <Ionicons name="checkmark-circle-outline" size={30} color="black" />
+          </TouchableOpacity>
         </View>
       </View>
     );
