1c11f75ec5a60145c96f0e2fe553896689cc9ffc
Jacek Bannach <jacek.bannach@haw-hamburg.de>
2023-06-02 18:12:56 +0200

    remove idea :(

--- __tests__/navigation/MainNavigation.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer'
-
-import MainNavigation from '../../components/navigations/MainNavigation';
-
-it('renders correctly', () => {
-  const tree = renderer.create(<MainNavigation />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
