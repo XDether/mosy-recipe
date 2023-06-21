024832da50c9c7b4a8b924d7338bd745ee5f2c9e
Jacek Bannach <jacek.bannach@haw-hamburg.de>
2023-06-21 15:08:28 +0200

    fix test data

--- components/navigations/MainNavigation.js
+++ components/navigations/MainNavigation.js
@@ -27,13 +27,13 @@ export default function MainNavigation ()
         {amount:'3',ingredient:'beef'},
         {amount:'3',ingredient:'Salt'}
       ]
-      ,[{instruction:'Just Put that salt on da beef'}],'a beefy recipe',"3hrs","3port"),
+      ,['Just Put that salt on da beef'],'a beefy recipe',"3hrs","3port"),
       new Recipe(1,'Food','Beef',
       [
         {amount:'2',ingredient:'beef'},
         {amount:'3',ingredient:'Salt'}
       ]
-      ,[{instruction:'Just Put that salt on da beef'}],'a beefuy recipe',"3hrs","3port")])
+      ,['Just Put that salt on da beef'],'a beefuy recipe',"3hrs","3port")])
     }
     createTestData();
   }, []);
