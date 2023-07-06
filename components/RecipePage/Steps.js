f4ba5ef49bb4dea723c420a9a57a5b528ad4eb74
Jacek Bannach <jacek.bannach@haw-hamburg.de>
2023-07-06 14:22:53 +0200

    small changes

--- components/RecipePage/Steps.js
+++ components/RecipePage/Steps.js
@@ -5,7 +5,7 @@ import { FlatList } from "react-native";
 
 export default function Steps({text}){
     return (
-    <View style={{height: Dimensions.get("window" ).height / 2.2}}>
+    <View style={{height: Dimensions.get("window" ).height / 2}}>
         {/*<Text style={{fontSize: 20, fontWeight:"bold",marginBottom: 10}}>Zubereitung</Text>*/}
         <FlatList data = {text}
             renderItem={({item}) => { return <Text style={{fontSize:16, fontWeight:600}}>{item}</Text>}}
--- components/RecipePage/ingredientList/IngredientList.js
+++ components/RecipePage/ingredientList/IngredientList.js
@@ -6,7 +6,7 @@ import { Dimensions } from "react-native";
 export default function IngredientList({ingredients}){
 
     return (
-    <View style = {{height: Dimensions.get("window").height /3.5}}>
+    <View style = {{height: Dimensions.get("window").height /3.2}}>
         <Text style={{fontSize: 20, fontWeight:"bold",marginBottom: 10}}>Zutaten</Text>
         <FlatList data = {ingredients}
         renderItem={({item}) => <Entry amount={item.amount} unit={item.unit} ingredient={item.ingredient}></Entry>}
